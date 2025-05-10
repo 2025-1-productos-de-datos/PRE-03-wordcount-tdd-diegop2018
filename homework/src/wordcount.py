0456dacca4d138f1a14ffa659fe4a18b0d8235e0
diegop2018 <36285502+diegop2018@users.noreply.github.com>
2025-05-10 09:46:06 -0500

    TDD INICIAL

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,12 @@
+# Ejemplo de uso:
+# python3 wordcount.py -f ../data/wordcount.txt -o ../data/wordcount.out
 
+import sys
+
+from homework.src._internals.parse_args import parse_args
+from homework.src._internals.read_all_lines import read_all_lines
+
+
+def main():
+    input_folder, output_folder = parse_args()
+    read_all_lines(input_folder)
